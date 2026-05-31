4fc7475bb3beccada242d0473a07cb9ba871983d
김지윤 <blue201232@gmail.com>
2026-05-31 09:18:44 +0900

    pipeline 수정

--- Pipeline.py
+++ Pipeline.py
@@ -4,7 +4,7 @@
 import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 from sklearn.preprocessing import StandardScaler, RobustScaler
 
 
@@ -13,97 +13,59 @@ df = pd.read_csv('Bronze\\adult.csv')
 x = df.drop('income', axis=1) # 입력 데이터
 y = df['income'] # 정답 데이터
 
-# # 결측치 처리 비교
-# # Most_Frequent Imputation
-# most_imputer = SimpleImputer(strategy='most_frequent')
-
-# x_most = pd.DataFrame(most_imputer.fit_transform(x), columns=x.columns)
-
-# # Drop Na
-# df_dropna = df.dropna().copy()
-
-# x_dropna = df_dropna.drop('income', axis=1)
-# y_dropna = df_dropna['income']
-
-# # 범주형 인코딩 비교
-# # One-Hot Encoding
-# oh = OneHotEncoder()
-# drop_of = oh.fit_transform(x_dropna.select_dtypes(include=['object']))
-# most_of = oh.fit_transform(x_most.select_dtypes(include=['object']))
-
-# # Label Encoding
-# le = LabelEncoder()
-# for col in x_dropna.select_dtypes(include=['object']).columns:
-#     x_dropna[col] = le.fit_transform(x_dropna[col])
-# for col in x_most.select_dtypes(include=['object']).columns:
-#     x_most[col] = le.fit_transform(x_most[col])
-
-# # 스케일링 비교 (StandardScaler, RobustScaler)
-# # 1. StandardScaler
-# scaler_most = StandardScaler()
-# scaler_dropna = StandardScaler()
-# x_dropna_scaled = scaler_dropna.fit_transform(x_dropna)
-# x_most_scaled = scaler_most.fit_transform(x_most)
-# # 2. RobustScaler
-# robust_dropna = RobustScaler()
-# robust_most = RobustScaler()
-# x_dropna_robust = robust_dropna.fit_transform(x_dropna)
-# x_most_robust = robust_most.fit_transform(x_most)
-
-# 파이프라인 구축
-df_most_one_standard = Pipeline([
-    ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('encoder', OneHotEncoder()),
-    ('scaler', StandardScaler())
-])
-
-df_dropna_one_standard = Pipeline([
-    ('dropna', 'passthrough'), # Drop Na는 별도의 단계로 처리
-    ('encoder', OneHotEncoder()),
-    ('scaler', StandardScaler())
-])
-
-df_most_one_robust = Pipeline([
-    ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('encoder', OneHotEncoder()),
-    ('scaler', RobustScaler())
-])
-
-df_dropna_one_robust = Pipeline([
-    ('dropna', 'passthrough'), # Drop Na는 별도의 단계로 처리
-    ('encoder', OneHotEncoder()),
-    ('scaler', RobustScaler())
-])
-
-df_most_label_standard = Pipeline([
-    ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('encoder', LabelEncoder()), # Label Encoding은 별도의 단계로 처리
-    ('scaler', StandardScaler())
-])
-
-df_most_label_robust = Pipeline([
-    ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('encoder', LabelEncoder()), # Label Encoding은 별도의 단계로 처리
-    ('scaler', RobustScaler())
-]) 
-
-df_dropna_label_standard = Pipeline([
-    ('dropna', 'passthrough'), # Drop Na는 별도의 단계로 처리
-    ('encoder', LabelEncoder()), # Label Encoding은 별도의 단계로 처리 
-    ('scaler', StandardScaler())
-])
-
-df_dropana_label_robust = Pipeline([
-    ('dropna', 'passthrough'), # Drop Na는 별도의 단계로 처리
-    ('encoder', LabelEncoder()), # Label Encoding은 별도의 단계로 처리 
-    ('scaler', RobustScaler())
-])
-
 # 파생 변수 생성 (스케일링 먼저 수행하면 변수 간의 단위 차이로 인한 편향 방지 가능)
 # 순자본 생성
 x['net_capital'] = x['capital-gain'] - x['capital-loss']
 # 근무 시간 구분
 x['work_intensity'] = pd.cut(x['hours-per-week'], bins=[0, 20, 40, 60, 100], labels=['part','full','overtime','extreme'])
+
+x_drop = x.dropna() # Drop Na는 별도의 단계로 처리
+y_drop = y.loc[x_drop.index] # Drop Na에 해당하는 인덱스에 맞춰 y도 정렬
+
+# 파이프라인 구축
+df_most_one_standard = Pipeline([
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('encoder', OneHotEncoder(handle_unknown='ignore')),
+    ('scaler', StandardScaler(with_mean=False)) # StandardScaler는 평균이 0이 되도록 조정하므로 with_mean=False로 설정하여 sparse matrix 지원
+])
+
+df_dropna_one_standard = Pipeline([
+    ('encoder', OneHotEncoder(handle_unknown='ignore')),
+    ('scaler', StandardScaler(with_mean=False))
+])
+
+df_most_one_robust = Pipeline([
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('encoder', OneHotEncoder(handle_unknown='ignore')),
+    ('scaler', RobustScaler())
+])
+
+df_dropna_one_robust = Pipeline([
+    ('encoder', OneHotEncoder(handle_unknown='ignore')),
+    ('scaler', RobustScaler())
+])
+
+df_most_label_standard = Pipeline([
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('encoder', OrdinalEncoder()), # Label Encoding은 별도의 단계로 처리
+    ('scaler', StandardScaler(with_mean=False))
+])
+
+df_most_label_robust = Pipeline([
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('encoder', OrdinalEncoder()), # Label Encoding은 별도의 단계로 처리
+    ('scaler', RobustScaler())
+]) 
+
+df_dropna_label_standard = Pipeline([
+    ('encoder', OrdinalEncoder()), # Label Encoding은 별도의 단계로 처리 
+    ('scaler', StandardScaler(with_mean=False))
+])
+
+df_dropna_label_robust = Pipeline([
+    ('encoder', OrdinalEncoder()), # Label Encoding은 별도의 단계로 처리 
+    ('scaler', RobustScaler())
+])
 
 # 데이터 저장
 x.to_csv('Silver\\adult_features.csv', index=False)
